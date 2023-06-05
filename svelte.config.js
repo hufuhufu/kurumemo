11cbf080a1e7aa272a1b742259c920a2cd0de7a8
Arif Hermawan <arif.hermawan12@gmail.com>
2023-06-05 22:58:52 +0800

    build: install shadcn-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			'$components/*': 'src/lib/components/*'
+		}
 	},
-
 	preprocess: [
 		preprocess({
 			postcss: true
 		})
 	]
 };
-
 export default config;
